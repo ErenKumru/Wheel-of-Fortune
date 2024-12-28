ea050ed0e5a8f2792ecdb07fc32506fbdb30b961
Eren Kumru <eren_kumru@hotmail.com>
2024-12-28 14:36:56 +0300

    ADD Vector2 Resizing Utility

--- Wheel of Fortune - Vertigo Games Demo/Assets/Scripts/Utility/Vector2Utility.cs
+++ Wheel of Fortune - Vertigo Games Demo/Assets/Scripts/Utility/Vector2Utility.cs
@@ -9,4 +9,24 @@ public static class Vector2Utility
         vector.x = xValue;
         return vector;
     }
+
+    public static Vector2 SetYValue(this Vector2 vector, float yValue)
+    {
+        vector.y = yValue;
+        return vector;
+    }
+
+    public static Vector2 ResizeHeightByTextureSize(this Vector2 vector, Texture2D texture)
+    {
+        float height = texture.height * vector.x / texture.width;
+        vector = SetYValue(vector, height);
+        return vector;
+    }
+
+    public static Vector2 ResizeWidthByTextureSize(this Vector2 vector, Texture2D texture)
+    {
+        float width = texture.width * vector.y / texture.height;
+        vector = SetXValue(vector, width);
+        return vector;
+    }
 }
